0ed6cd8a1aed292277456c434cb2acdf398e30a0
Atul Sharma <atul.sharma.123647@gmail.com>
2025-11-17 01:32:24 +0530

    updated firebase

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -12,5 +13,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
 });
